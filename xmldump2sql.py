f06dc1162f250ab401aa010cd9d6a31540ed4105
Felix Möller <git@felix-moeller.net>
2017-04-07 02:59:28 +0200

    Improve xmldump2sql.py

--- xmldump2sql.py
+++ xmldump2sql.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from pprint import pprint
+from models import Base, TemporalLocation, Bike, Station
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-from TrackABike import TrackABike, read_xml_dumps
+# from TrackABike import TrackABike, read_xml_dumps
+#
+# track_a_bike = TrackABike()
+# for data in read_xml_dumps():
+#     track_a_bike.load_xml(data)
 
-track_a_bike = TrackABike()
-for data in read_xml_dumps():
-    track_a_bike.load_xml(data)
-    pprint(track_a_bike.stations)
+engine = create_engine('sqlite:///:memory:', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def create_db():
+    Base.metadata.create_all(engine)
+
+
+def sample_data():
+    station = Station(id=3, name='lelkek', lat=32.551, lng=64.112,
+                      is_outside=False)
+    bike = Bike(id=4, can_be_rented=False, can_be_returned=False, version=2,
+                marke_id=23, marke_name="Kekrad", is_pedelec=False)
+
+    location = TemporalLocation(time_from=datetime.now(),
+                                time_to=datetime.now())
+    location.bike = bike
+    location.station = station
+
+    return station, bike, location
+
+
+station, bike, location = sample_data()
+session.add(location)
+# session.commit()
+
+# TODO Fix bike_id='None' and station_id='None'
+print(location)
