ef844efb892c10b6dd3c4a1685526e20155854eb
Soeren Wegener <wegener92@gmail.com>
2017-04-08 02:03:33 +0200

    +x

--- xmldump2neo4j.py
+++ xmldump2neo4j.py
@@ -76,7 +76,7 @@ def create_moving_bikes_relations(session):
 
 
 if __name__ == '__main__':
-    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "Eiqu3soh"))
+    driver = GraphDatabase.driver('bolt://localhost:7687', auth=basic_auth('neo4j', 'Eiqu3soh'))
     session = driver.session()
     wipe_database(session)
     create_stations(session)
