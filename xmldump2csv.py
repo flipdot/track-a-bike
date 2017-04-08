61c71e2688efae617195de4085684856e239b089
Soeren Wegener <wegener92@gmail.com>
2017-04-08 16:52:40 +0200

    Changed CSV headers to better fit neo4j

--- xmldump2csv.py
+++ xmldump2csv.py
@@ -15,8 +15,9 @@ def create_stations():
     track_a_bike.load_xml(data)
     with open(os.path.join(CSV_DIRECTORY, 'stations.csv'), 'w') as f:
         fieldnames = ['id', 'name', 'lat', 'lng', 'is_outside']
-        writer = csv.DictWriter(f, fieldnames)
-        stations = map(lambda x: {key: x[key] for key in fieldnames}, track_a_bike.stations.values())
+        headernames = {'id': 'id:ID(Station)'}
+        writer = csv.DictWriter(f, [headernames.get(x, x) for x in fieldnames])
+        stations = map(lambda x: {headernames.get(key, key): x[key] for key in fieldnames}, track_a_bike.stations.values())
         writer.writeheader()
         writer.writerows(stations)
 
@@ -24,6 +25,7 @@ def create_stations():
 def create_bikes():
     track_a_bike = TrackABike()
     fieldnames = ['number', 'version', 'marke_id', 'marke_name', 'is_pedelec']
+    headernames = {'number': 'id:ID(Bike)'}
     bikes = {}
     i = 0
     for timestamp, data in read_xml_dumps():
@@ -33,20 +35,20 @@ def create_bikes():
         # minute, so we just process a dataset every hour
         if i % 60:
             continue
-        print(timestamp)
+        # print(timestamp)
         track_a_bike.load_xml(data)
         for station in track_a_bike.stations.values():
             # print(station['free_bikes'])
             update = {}
             for bike in station['free_bikes']:
-                update[bike['number']] = {key: bike[key] for key in fieldnames}
+                update[bike['number']] = {headernames.get(key, key): bike[key] for key in fieldnames}
             bikes.update(update)
             # bikes.update({free_bikes['number']: free_bikes[key] for key in fieldnames})
     with open(os.path.join(CSV_DIRECTORY, 'bikes.csv'), 'w') as f:
-        writer = csv.DictWriter(f, fieldnames)
+        writer = csv.DictWriter(f, [headernames.get(x, x) for x in fieldnames])
         writer.writeheader()
         bikes_list = list(bikes.values())
-        bikes_list.sort(key=lambda x: x['number'])
+        bikes_list.sort(key=lambda x: x['id:ID(Bike)'])
         writer.writerows(bikes_list)
 
 
@@ -54,8 +56,11 @@ if __name__ == '__main__':
     if not os.path.exists(CSV_DIRECTORY):
         os.makedirs(CSV_DIRECTORY)
     track_a_bike = TrackABike()
+    print('Creating stations.csv…')
     create_stations()
+    print('Creating bikes.csv…')
     create_bikes()
+    print('Creating ')
     # for timestamp, data in read_xml_dumps():
     #     track_a_bike.load_xml(data)
     #     print(track_a_bike.stations)
