f8803d5bbd4b9a8de0a9efb5136997a5812b7828
Soeren Wegener <wegener92@gmail.com>
2017-04-09 04:48:51 +0200

    Fixed plotting experiments to match new schema

--- neo4j2plot.py
+++ neo4j2plot.py
@@ -21,25 +21,25 @@ if __name__ == '__main__':
         print(station)
         # nstations.items():
         #     print(len(station['free_bikes']))
-    # fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
     #
-    # driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "Eiqu3soh"))
-    # session = driver.session()
-    # result = list(session.run("MATCH (a) RETURN a.name as name"))
-    # stations = list(map(lambda x: x['name'], result))
-    # result = list(session.run("MATCH (a)-[r]->(b) RETURN a.name as name, r.timestamp as timestamp;"))
-    # x = list(map(lambda x: datetime.fromtimestamp(x['timestamp']).hour, result))
-    # y = list(map(lambda x: stations.index(x['name']), result))
-    # # scale =
-    # ax.scatter(x, y, alpha=0.05, edgecolors='none')
-    #
-    # result = list(session.run("MATCH (a)-[r]->(b) RETURN b.name as name, r.timestamp as timestamp;"))
-    # x = list(map(lambda x: datetime.fromtimestamp(x['timestamp']).hour + 0.5, result))
-    # y = list(map(lambda x: stations.index(x['name']), result))
-    # # scale =
-    # ax.scatter(x, y, alpha=0.05, color='red', edgecolors='none')
-    #
-    # ax.legend()
-    # ax.grid(True)
-    #
-    # plt.show()
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "Eiqu3soh"))
+    session = driver.session()
+    result = list(session.run("MATCH (a) RETURN a.name as name"))
+    stations = list(map(lambda x: x['name'], result))
+    result = list(session.run("MATCH (a)-[r:BIKE_MOVED]->(b) RETURN a.name as name, r.timestamp_start as timestamp;"))
+    x = list(map(lambda x: datetime.fromtimestamp(x['timestamp']).hour, result))
+    y = list(map(lambda x: stations.index(x['name']), result))
+    # scale =
+    ax.scatter(x, y, alpha=0.05, edgecolors='none')
+
+    result = list(session.run("MATCH (a)-[r:BIKE_MOVED]->(b) RETURN b.name as name, r.timestamp_end as timestamp;"))
+    x = list(map(lambda x: datetime.fromtimestamp(x['timestamp']).hour + 0.5, result))
+    y = list(map(lambda x: stations.index(x['name']), result))
+    # scale =
+    ax.scatter(x, y, alpha=0.05, color='red', edgecolors='none')
+
+    ax.legend()
+    ax.grid(True)
+
+    plt.show()
