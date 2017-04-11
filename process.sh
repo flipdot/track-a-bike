b957ebe2caea91d63f0fac536609505a4aa5383b
Soeren Wegener <wegener92@gmail.com>
2017-04-11 19:13:09 +0200

    sudo for deleting old graph.db

--- process.sh
+++ process.sh
@@ -26,11 +26,11 @@ es() {
 es "Creating CSV files…" &&
 python xmldump2csv.py &&
 es "Removing old graph.db…" &&
-rm -rf $HOME/neo4j/databases/graph.db/ &&
+sudo rm -rf $HOME/neo4j/databases/graph.db/ &&
 es "Creating Neo4J graph.db…" &&
 sudo ./csv2neo4j.sh &&
 es "Starting Neo4J…" &&
-sudo docker run -d -p 7687:7687 -p 7474:7474 -v /home/soeren/neo4j:/data --name neo4j neo4j &&
+sudo docker run -d -p 7687:7687 -p 7474:7474 -v $HOME/neo4j:/data --name neo4j neo4j &&
 sleep 10 && # Make sure the database is ready
 es "Finding and marking bike transports…" &&
 python mark_transporters.py &&
