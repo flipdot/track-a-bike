4eb77b69db677cc2e871275d4a0fe60672a5880c
Felix Möller <git@felix-moeller.net>
2017-04-11 02:18:28 +0200

    Fix unpacking in download script

--- download.sh
+++ download.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
+# Get date
+gd() {
+    date --date="${1%%.*}" +%s
+}
+
 cd dumps
-rsync ajvar:~/konraddump/*.tar.xz .
-# TODO: unp only new tars
-unp *.tar.xz
+# Last date via file name
+ld=$(gd $(basename $(find -type f -name '*.tar.xz' | sort | tail -1)))
+
+# Transfer
+rsync -a ajvar:~/konraddump/*.tar.xz .
+
+for f in *.tar.xz; do
+    # File date
+    fd=$(gd "$(basename $f)")
+    [[ $((fd-ld)) -gt 0 ]] && unp "$f.tar.xz"
+done
+true
