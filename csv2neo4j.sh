a18d0d8899de077f02ef923f836c80de0a255e6e
Soeren Wegener <wegener92@gmail.com>
2017-04-15 16:55:49 +0200

    Uncommented xml2csv

--- csv2neo4j.sh
+++ csv2neo4j.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 sudo docker run -it --rm -v $PWD/csv:/csv -v $NEO4J_DIR:/data neo4j \
-    /var/lib/neo4j/bin/neo4j-admin import --id-type integer \
-    --nodes:Station /csv/stations.csv --nodes:Bike \
-    /csv/bikes.csv --relationships:LOCATED_AT \
-    /csv/bike_positions.csv --relationships:BIKE_MOVED \
-    /csv/bike_movements.csv
+    /var/lib/neo4j/bin/neo4j-admin import \
+    --id-type integer \
+    --nodes:Station /csv/stations.csv \
+    --nodes:Bike /csv/bikes.csv \
+    --relationships:LOCATED_AT /csv/bike_positions.csv \
+    --relationships:BIKE_MOVED /csv/bike_movements.csv
